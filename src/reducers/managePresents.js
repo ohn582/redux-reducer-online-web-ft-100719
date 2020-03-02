b5a63d84f1c2cbc722b98ce3bab305711f7c68cc
Neal JIn Oh <no4259@gmail.com>
2020-03-02 22:10:50 +0000

    Automatically backed up by Learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,3 +1,10 @@
 export function managePresents(state, action){
-  
+  action = {
+    type: "ADD_FRIEND",
+    friend: {
+      name: "Chrome Boi"
+      homewtown: "NYC",
+      id: 1
+    }
+  }
 }
